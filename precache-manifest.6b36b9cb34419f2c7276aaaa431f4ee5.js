f1204cdc9b20d2e945d78e62dab705bd2f399078
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-08-07 16:16:42 -0300

    Updates

---
+++ precache-manifest.6b36b9cb34419f2c7276aaaa431f4ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815e8005157a57d250a16f68c2c05273",
+    "url": "/LizzardMedeiros/Reactflix/index.html"
+  },
+  {
+    "revision": "2ac413ae541129d4f867",
+    "url": "/LizzardMedeiros/Reactflix/static/css/main.71ec20e9.chunk.css"
+  },
+  {
+    "revision": "8c9de74cb59359d879a1",
+    "url": "/LizzardMedeiros/Reactflix/static/js/2.580d2467.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LizzardMedeiros/Reactflix/static/js/2.580d2467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac413ae541129d4f867",
+    "url": "/LizzardMedeiros/Reactflix/static/js/main.143907b0.chunk.js"
+  },
+  {
+    "revision": "2966694c73b50c237468",
+    "url": "/LizzardMedeiros/Reactflix/static/js/runtime-main.635e88ff.js"
+  },
+  {
+    "revision": "15389af31963027e9172f3b1c78962d8",
+    "url": "/LizzardMedeiros/Reactflix/static/media/logo.15389af3.svg"
+  }
+]);
